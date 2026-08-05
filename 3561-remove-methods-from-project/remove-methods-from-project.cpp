40f566198fbc371ba3f9bef73bea7989cc871506
Shah Diya <153583189+diya2809@users.noreply.github.com>
2026-08-05 18:36:26 +0530

    Time: 189 ms (59.31%) | Memory: 297.7 MB (90.00%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+
+        for (auto &e : invocations) {
+            graph[e[0]].push_back(e[1]);
+        }
+
+        vector<bool> suspicious(n, false);
+
+        // DFS to mark all suspicious methods
+        stack<int> st;
+        st.push(k);
+        suspicious[k] = true;
+
+        while (!st.empty()) {
+            int u = st.top();
+            st.pop();
+
+            for (int v : graph[u]) {
+                if (!suspicious[v]) {
+                    suspicious[v] = true;
+                    st.push(v);
+                }
+            }
+        }
+
+        // Check if any outside method invokes a suspicious method
+        for (auto &e : invocations) {
+            int u = e[0], v = e[1];
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++) ans.push_back(i);
+                return ans;
+            }
+        }
+
+        // Return remaining methods
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
